f7af3a7f205c269ca30ea184cfb6ffb834c991ba
QUEVEDO <alexandre.quev@gmail.com>
2023-10-12 12:54:04 -0700

    Task 5 Hello printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Affiche le texte with proper grammar, but the outcome is
- * a piece of art followed by a new line
- * Return: Always zero
- */
-int main()
+* main - Affiche le texte with proper grammar, but the outcome is
+* a piece of art followed by a new line
+* Return: Always zero
+*/
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
     
-    return (0) ;
+return (0) ;
 }
