0b37c9d5f1b5e7d1574fe5704287de3fff2133d4
QUEVEDO <alexandre.quev@gmail.com>
2023-10-12 12:31:17 -0700

    Task 4 Hello Puts 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 /*Fonction principale*/
 int main(void) {
 
     /* Afficher un texte */
-    puts("Programming is like building a multilingual puzzle");    
+    puts("/"Programming is like building a multilingual puzzle");    
       
         
     return 0;
