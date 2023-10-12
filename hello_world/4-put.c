6a648cc31e135cf63caadc4c546943fcc0c517d6
QUEVEDO <alexandre.quev@gmail.com>
2023-10-12 12:27:04 -0700

    Task 4 Hello puts 3

--- hello_world/4-put.c
+++ hello_world/4-put.c
@@ -1,19 +1,11 @@
-/*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+/*Fonction principale*/
+int main(void) {
 
-/**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
+    /* Afficher un texte */
+    puts("Programming is like building a multilingual puzzle");    
+      
+        
+    return 0;
 }
