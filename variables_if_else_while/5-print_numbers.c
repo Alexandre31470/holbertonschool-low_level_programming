5c3812531e53d088651732a2cff09d1797d206bc
QUEVEDO <alexandre.quev@gmail.com>
2023-10-16 06:28:05 -0700

    Task 5 Numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -14,10 +14,7 @@ int main(void)
     {
         putchar(num + '0');
         if (num < 9)
-        {
-            
-            putchar(' ');
-        }
+      
         num++;
     }
     putchar('\n');
