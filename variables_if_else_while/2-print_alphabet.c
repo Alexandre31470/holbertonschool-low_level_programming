47af9b078dd3a4548f1be7dc95077eb271ecfe5a
QUEVEDO <alexandre.quev@gmail.com>
2023-10-16 04:28:12 -0700

    Task 2 I sometimes suffer from insomnia

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Afficher alphabet en minuscule
+ *
+ *Return: Always 0 (success)
+ */
+ int main (void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+{
+	putchar(alphabet);
+	alphabet++;
+}
+	putchar('\n');
+	return (0);
+}
