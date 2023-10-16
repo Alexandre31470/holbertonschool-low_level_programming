1851b8345803e6af6905631a2fb8633ab886413f
QUEVEDO <alexandre.quev@gmail.com>
2023-10-16 12:35:36 -0700

    Task 9 Patience Persistence

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
-
 /**
-* main Print all possible combinations of single-digit numbers
-*  
+*main - Print all possible combinations of single-digit numbers
 * 
-* 
-* 
-* Return: 0 (success)
+*Return: 0 (success)
 */
 int main(void)
 {
@@ -16,10 +12,9 @@ for (x = 48; x < 58; x++)
 {
 putchar(x);
 if (x < 57)
-{
+
 putchar(44);
 putchar(32);
-}
 }
 putchar('\n');
 return (0);
