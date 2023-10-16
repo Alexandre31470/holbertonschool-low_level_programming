9c49e313b34934ba87b804f711ec41a6426f2893
QUEVEDO <alexandre.quev@gmail.com>
2023-10-16 05:33:26 -0700

    Task 4 When i was having that alphabet soup

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Afficher alphabet en minuscule
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z')
+	if ((alphabet == 'q') || (alphabet == 'e'))
+	alphabet++;	
+	else 
+{
+	putchar(alphabet);
+	alphabet++;
+}
+	putchar('\n');
+	return (0);
+}
