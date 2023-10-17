0f7ab420a42fbca209cda24dc1d5b8e691d7caf4
QUEVEDO <alexandre.quev@gmail.com>
2023-10-17 04:19:04 -0700

    Task 1 Sometimes suffer from insomnia

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Afficher alphabet en minuscule
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+_putchar('a');
+_putchar('z');
+_putchar('\n');
+return (0);
+}
