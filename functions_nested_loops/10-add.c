5ab4e41fef2bb60663dcfa7b730e2e8c678cae34
QUEVEDO <alexandre.quev@gmail.com>
2023-10-18 01:51:54 -0700

    Task 10 a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* add(int, int) - Adds two integers and returns the result.
+*
+* Return: Always 0.
+*/
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
