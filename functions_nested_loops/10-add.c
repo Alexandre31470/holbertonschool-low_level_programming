d7423c81e774beea0d2c4fedfc785de3442c1a29
QUEVEDO <alexandre.quev@gmail.com>
2023-10-18 01:56:27 -0700

    Task 10 a + b

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
 * add - Adds two integers and returns the result.
-*num1 - Number 1
-*num2 - Number 2
+*@num1 - Number 1
+*@num2 - Number 2
 * Return: Number 1 * Number 2.
 */
 int add(int num1, int num2)
