84613f34f50bc0028812475f12ee34ac04bd787b
QUEVEDO <alexandre.quev@gmail.com>
2023-10-20 02:17:17 -0700

    Task 9 Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - Function replace for Fizz Buzz.
+*Return: 0
+*/
+int main(void)
+{
+int i = 1;
+
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else if (i % 5 == 0)
+{
+if (i == 100)
+{
+printf("Buzz");
+printf("\n");
+}
+else
+printf("Buzz");
+}
+else if (i % 3 == 0)
+printf("Fizz");
+else
+printf("%d", i);
+i++;
+}
+return (0);
+}
