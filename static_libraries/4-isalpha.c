d91955a492e1dcfcc7209f204d50c9598a86b687
QUEVEDO <alexandre.quev@gmail.com>
2023-10-31 09:05:18 -0700

    Task 0 Project 2 Statics Libraries

--- static_libraries/4-isalpha.c
+++ static_libraries/4-isalpha.c
@@ -1,5 +1,15 @@
 #include "main.h"
+/**
+ * _isalpha - Check if character is a alphabet character.
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
+ */
+
 int _isalpha(int c)
 {
-	return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+
+	else
+		return (0);
 }
